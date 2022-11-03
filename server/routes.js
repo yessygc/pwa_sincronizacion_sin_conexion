b461f6a0d5b8ae40802a4ff836ddf2cd5e89fbef
yessygc <yessgarciacampos@gmail.com>
2022-11-02 21:24:51 -0600

    API REST_get mensajes

--- server/routes.js
+++ server/routes.js
@@ -3,14 +3,23 @@ var express = require('express');
 var router = express.Router();
 
 
+const mensajes = [
 
+  {
+  _id: 'XXX',
+  user: 'spiderman',
+  mensaje: 'Hola spiderman' 
+  }
+
+];
 
 
 
 
 // Get mensajes
 router.get('/', function (req, res) {
-  res.json('Obteniendo mensajes');
+  // res.json('Obteniendo mensajes');
+  res.json( mensajes );
 });
 
 
